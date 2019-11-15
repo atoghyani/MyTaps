c8d0f94e2955af389f42653b6349c75636fedc1f
atoghyani <ariantoghyani@gmail.com>
2019-11-15 10:16:09 -0800

    Update myvsinstall.rb

--- myvsinstall.rb
+++ myvsinstall.rb
@@ -1,10 +1,10 @@
 class Myvsinstall < Formula
   version '8.3.8.8'
   sha256 '0a3cd5a850a387fb115fd51e45c4df6b966da815ec20ad454327ee2e34ed1807'
-  # dl.xamarin.com/VsMac was verified as official when first introduced to the cask
   url "https://dl.xamarin.com/VsMac/VisualStudioForMac-#{version}.dmg"
   name 'Visual Studio for Mac'
   homepage 'https://www.visualstudio.com/vs/visual-studio-mac/'
+  
   bottle :unneeded
 
   auto_updates true
