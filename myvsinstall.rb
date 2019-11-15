c620a66147f1af2f69f3d793542d55fa75664009
atoghyani <ariantoghyani@gmail.com>
2019-11-15 09:38:46 -0800

    Create myvsinstall.rb

---
+++ myvsinstall.rb
@@ -0,0 +1,35 @@
+cask 'myvsinstall' do
+  version '8.3.8.8'
+  sha256 '0a3cd5a850a387fb115fd51e45c4df6b966da815ec20ad454327ee2e34ed1807'
+
+  # dl.xamarin.com/VsMac was verified as official when first introduced to the cask
+  url "https://dl.xamarin.com/VsMac/VisualStudioForMac-#{version}.dmg"
+  appcast 'https://docs.microsoft.com/en-us/visualstudio/releasenotes/vs2019-mac-relnotes',
+          configuration: version.major_minor_patch
+  name 'Visual Studio for Mac'
+  homepage 'https://www.visualstudio.com/vs/visual-studio-mac/'
+
+  auto_updates true
+  depends_on macos: '>= :sierra'
+  depends_on cask: 'mono-mdk'
+  depends_on cask: 'xamarin-android'
+  depends_on cask: 'xamarin-ios'
+  depends_on cask: 'homebrew/cask-versions/adoptopenjdk8'
+  depends_on cask: 'android-sdk'
+  depends_on cask: 'android-ndk'
+
+  app 'Visual Studio.app'
+
+  zap trash: [
+               '/Applications/Xamarin Workbooks.app',
+               '/Applications/Xamarin Profiler.app',
+               '~/Library/Application Support/VisualStudio',
+               '~/Library/Application Support/CrashReporter/VisualStudio*',
+               '~/Library/Caches/VisualStudio',
+               '~/Library/Logs/VisualStudio',
+               '~/Library/Preferences/Visual*Studio',
+               '~/Library/Preferences/Xamarin',
+               '~/Library/Developer/Xamarin',
+               '~/Library/VisualStudio',
+             ]
+end
